f9311593c526c44a7b3ba022a60ad91137a5b531
Andrii Tiutiakh <andrii.tiutiakh@globallogic.com>
2018-12-04 17:28:40 +0200

    print and count function implementation

--- main.cpp
+++ main.cpp
@@ -3,26 +3,31 @@
 class Bank {
 private:
     float _precent;
-    float _period;
-    float _amount;
+    int _period;
+    float _present_amount;
+    float _future_amount;
 public:
-    Bank();
-    Bank(float precent, float period, float amount) : _precent(precent), _period(period), _amount(amount){}
+    Bank() : _precent(0), _period(0), _present_amount(0), _future_amount(0){}
+    Bank(float precent, int period, float amount) : _precent(precent), _period(period), _present_amount(amount), _future_amount(0){}
     void print();
     void count();
 };
 
 int main(int argc, char *argv[]) {
     std::cout << "Hey" << std::endl;
-    Bank bank(1,2,3);
-    bank.print();
+    Bank bank(0.043,6,1500);
     bank.count();
+    bank.print();
     return 0;
 }
 
 void Bank::print() {
-    std::cout << "print" << std::endl;
+    std::cout << _future_amount << std::endl;
 }
 void Bank::count() {
-    std::cout << "count" << std::endl;
+    _precent++;
+    float multilier = _precent;
+    for(int i = 0; i < _period-1; ++i)
+        _precent *= multilier;
+    _future_amount = _present_amount * _precent;
 }
